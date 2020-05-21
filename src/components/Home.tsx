15bbd9f850b5153f661227660dd9af9eb10cf745
Athiiii <theivakulasingam@outlook.de>
2020-05-21 14:58:32 +0200

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
+import { Box, Container, Typography } from "@material-ui/core";
 import React, { Component } from "react";
-import { Button, Container, Box, Typography } from "@material-ui/core";
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -9,7 +9,7 @@ export class Home extends Component {
       <Container maxWidth="sm">
         <Box my={4}>
           <Typography variant="h4" component="h1" gutterBottom>
-            Athi Pen
+            Athi Pen Page
           </Typography>
         </Box>
       </Container>
