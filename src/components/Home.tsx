67632d90669a1956ce5296feceecb3e40a5e4d67
Athiiii <theivakulasingam@outlook.de>
2020-05-21 15:07:30 +0200

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,7 +9,7 @@ export class Home extends Component {
       <Container maxWidth="sm">
         <Box my={4}>
           <Typography variant="h4" component="h1" gutterBottom>
-            Athi Pen Page
+            Athi Pen Page I
           </Typography>
         </Box>
       </Container>
