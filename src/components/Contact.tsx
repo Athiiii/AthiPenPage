f3f8e1e522afe8f106da957110912be70f336c82
Athavan Theivakulasingham <44317288+Athiiii@users.noreply.github.com>
2020-06-02 23:26:53 +0200

    finish contact description part

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -13,6 +13,13 @@ export class Contact extends Component {
                         <span className="contact_header">
                             <h3 className="contact_header_first">Don't be a stranger</h3>
                             <h3 className="contact_header_second">just say hello</h3>
+
+                            <span className="contact_header_text">Feel free to get in touch with us. We are always optn to discuss new projects, creative ideas or opportunities to be part of your visions.</span>
+                            <span className="contact_header_help">Need help?</span>    
+                            <a href="mailto:contact_us@athipen.com" className="contact_header_mail">contact_us@athipen.com</a>
+                            <span className="contact_header_help">Feel like to rather talk?</span>    
+                            <p className="contact_header_phone">+41 79 123 456 78</p>
+
                         </span>
                     </Grid>
                     <Grid item xs={6}>
