a84fdcaff1d2280e2c77f7d9316d99ba9de8218b
Athavan Theivakulasingham <44317288+Athiiii@users.noreply.github.com>
2020-06-12 23:14:17 +0200

    athi pen front page 100vh

--- src/components/AthiPen.tsx
+++ src/components/AthiPen.tsx
@@ -31,28 +31,36 @@ export class AthiPen extends Component<{}, IAthiPenState> {
         return (
             <div className="athiPen">
                 <span className="athiPen_header">
-                    <div>
-                        <Typography variant="h2">The AthiPen</Typography>
-                        <Typography variant="h3">The next generation of pens</Typography>
-                        <span className="athiPen_header_colors">
-                            <div className={`athiPen_header_colors_item athiPen_header_colors_item-first ${firstColorClicked ? 'selected' : ''}`} onClick={this.firstColorClicked}>
-                                <span>
-                                    <div></div>
-                                </span>
-                            </div>
-                            <div className={`athiPen_header_colors_item athiPen_header_colors_item-second ${secondColorClicked ? 'selected' : ''}`} onClick={this.secondColorClicked}>
-                                <span>
-                                    <div></div>
-                                </span>
-                            </div>
-                        </span>
-                        <Button variant="contained" className="athiPen_header_button" startIcon={
-                            <FontAwesomeIcon size="lg" icon={"shopping-cart"} color={Color.Dark} className="mb-1 mr-0 ml-1" />
-                        }>Add to cart</Button>
-                        <Link href="#learn_more" className="athiPen_header_learn-more">Learn more</Link>
+                    <span>
+                        <div>
+                            <Typography variant="h2">The AthiPen</Typography>
+                            <Typography variant="h3">The next generation of pens</Typography>
+                            <span className="athiPen_header_colors">
+                                <div className={`athiPen_header_colors_item athiPen_header_colors_item-first ${firstColorClicked ? 'selected' : ''}`} onClick={this.firstColorClicked}>
+                                    <span>
+                                        <div></div>
+                                    </span>
+                                </div>
+                                <div className={`athiPen_header_colors_item athiPen_header_colors_item-second ${secondColorClicked ? 'selected' : ''}`} onClick={this.secondColorClicked}>
+                                    <span>
+                                        <div></div>
+                                    </span>
+                                </div>
+                            </span>
+                            <Button variant="contained" className="athiPen_header_button" startIcon={
+                                <FontAwesomeIcon size="lg" icon={"shopping-cart"} color={Color.Dark} className="mb-1 mr-0 ml-1" />
+                            }>Add to cart</Button>
+                            <Link href="#learn_more" className="athiPen_header_learn-more">Learn more</Link>
+                        </div>
+                        <img src={require("../assets/blue-pen.png")} alt="pen blue" className={`athiPen_header_img athiPen_header_img_${!firstColorClicked ? "disappeared" : ""}`} />
+                        <img src={require("../assets/orange-pen.png")} alt="pen orange" className={`athiPen_header_img athiPen_header_img_${!secondColorClicked ? "disappeared" : ""}`} />
+                    </span>
+                    <div className="athiPen_header_show-more">
+                        <a href="#">
+                            <p>Show more</p>
+                            <FontAwesomeIcon size="lg" icon={"angle-double-down"} color={Color.Dark} className="mb-1 mr-0 ml-1" />
+                        </a>
                     </div>
-                    <img src={require("../assets/blue-pen.png")} alt="pen blue" className={`athiPen_header_img athiPen_header_img_${!firstColorClicked ? "disappeared" : ""}`} />
-                    <img src={require("../assets/orange-pen.png")} alt="pen orange" className={`athiPen_header_img athiPen_header_img_${!secondColorClicked ? "disappeared" : ""}`} />
                 </span>
                 <span className="athiPen_ads">
                     <img src={require("../assets/Ad.png")} alt="Advertisement" className="athiPen_header_ad" />
